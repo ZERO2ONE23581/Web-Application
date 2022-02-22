4c02bd0a75bf644ca54bfc1624501967d3b9a1c8
aiden <zero2one23581@gmail.com>
2022-02-22 22:22:56 +0900

    todo (save)

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,11 @@
 /*
 <instruction>
 1. todo list를 html상에 나오도록 만든다.
+2. data를 local에 저장한다
 */
 const todoInput = todoForm.querySelector("#todoForm input");
 const todoList = document.getElementById("todo-list");
+const TODO_KEY = "todo-key";
 
 todoForm.addEventListener("submit", todoSubmit);
 
@@ -12,14 +14,15 @@ function todoSubmit(event) {
   const todo = todoInput.value;
   todoInput.value = null;
   showTodo(todo);
+  saveTodo(todo); //로컬데이터에 저장
 }
 
-function showTodo(todo) {
+function showTodo(event) {
   const li = document.createElement("li");
   todoList.appendChild(li);
   const span = document.createElement("span");
   li.appendChild(span);
-  span.innerText = todo;
+  span.innerText = event;
   const btn = document.createElement("button");
   li.appendChild(btn);
   btn.innerText = "❌";
@@ -29,4 +32,15 @@ function showTodo(todo) {
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+}
+
+//로컬에 데이터 보관 (locastorage!==database)
+function saveTodo(event) {
+  localStorage.setItem(TODO_KEY, event);
+}
+//로컬에 보관된 데이터 소환
+const savedTodo = localStorage.getItem(TODO_KEY);
+if (savedTodo !== null) {
+  //null인 경우 innerText가 null인채로 html에 표시된다.
+  showTodo(savedTodo);
 }
