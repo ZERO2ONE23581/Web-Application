8cd32924882e96ed8d3150ceaa79a46d7b9f302d
aiden <zero2one23581@gmail.com>
2022-03-18 09:00:42 +0900

    styles

--- js/bg.js
+++ js/bg.js
@@ -1,6 +1,9 @@
 const bgArr = ["001.jpeg", "002.jpeg", "003.jpeg", "004.jpeg", "005.jpeg"];
 
 const chosenBg = bgArr[parseInt(Math.random() * bgArr.length)];
+const bgImg = document.createElement("img");
+bgImg.src = `img/${chosenBg}`;
+document.body.appendChild(bgImg);
 
-const imgSrc = `url(img/${chosenBg})no-repeat center/cover`;
-document.body.style.background = imgSrc;
+// const imgSrc = `url(img/${chosenBg})no-repeat center/cover`;
+// document.body.style.background = imgSrc;
